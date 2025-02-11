87447a96cb29c7d311282c74d9a35fc571140eab
Taoheedbalogun <talk2taobal242@yahoo.com>
2025-02-11 13:21:09 +0100

    did some styling to the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import logo from "../assets/logo.jpg";
-
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <div className="container">
       <div className="wrapper">
         <div className="box">
-          {/* <Logo></Logo> */}
           <div className="time">Today 12pm - 9pm</div>
           <div className="note">Working Hours</div>
         </div>
         <div className="box">
-          {/* <Logo></Logo> */}
           <div className="time">Piastowska 4, Cracow</div>
           <div className="note">Find us</div>
         </div>
         <div className="box">
-          {/* <Logo></Logo> */}
           <div className="time">503 204 176</div>
           <div className="note">Call to Us</div>
         </div>
@@ -31,21 +29,21 @@ const Footer = () => {
         </div>
         <div className="navigation">
           <div className="nav">
-            <span>About us</span>
+            <span className="nav-list">About us</span>
             <a>
-              <FaFacebook />
+              <FaFacebook className="icon" />
             </a>
           </div>
           <div className="nav">
-            <span>Menu</span>
+            <span className="nav-list">Menu</span>
             <a>
-              <FaTwitter />
+              <FaTwitter className="icon" />
             </a>
           </div>
           <div className="nav">
-            <span>Contact</span>
+            <span className="nav-list">Contact</span>
             <a>
-              <FaInstagram />
+              <FaInstagram className="icon" />
             </a>
           </div>
         </div>
